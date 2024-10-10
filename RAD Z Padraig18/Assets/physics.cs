c7de60e907543828ce1af35a6972f49ce8bca15c
STUDENT Padraig F. O Brien <Padraig.F.OBrien@students.ittralee.ie>
2024-10-10 10:24:45 +0100

    Signed-off-by: STUDENT Padraig F. O Brien <Padraig.F.OBrien@students.ittralee.ie>

--- RAD Z Padraig18/Assets/physics.cs
+++ RAD Z Padraig18/Assets/physics.cs
@@ -2,25 +2,27 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class physics : MonoBehaviour
+public class Physics : MonoBehaviour
 {
-    Rigidbody rb; 
+    Rigidbody rb;
     // Start is called before the first frame update
     void Start()
-    {   
+    {
+
         rb = GetComponent<Rigidbody>();
         if (rb != null)
             print("Found Rigid Body");
         else
-            print("Could not find Rigid Body");
-        
+            print("Could Not Find The Rigid Body");
+
     }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.UpArrow))
-            rb.AddExplosionForce(5,
+            rb.AddExplosionForce(5000,
                 transform.position + new Vector3(0, -1, -1), 2);
+
     }
 }
